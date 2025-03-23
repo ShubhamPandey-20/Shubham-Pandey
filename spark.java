be3d94a8715a07dbeca52d586541c57300dbcd97
Shubham Pandey <shubhampan2222@gmail.com>
2025-03-23 15:59:44 +0530

    Done

--- spark.java
+++ spark.java
@@ -1,5 +1,9 @@
 class spark{
     public static void main(String[] args) {
         System.out.println("Hello");
+        m1();
     }
+    public static void m1(){
+    System.out.println(".()");
+}
 }
