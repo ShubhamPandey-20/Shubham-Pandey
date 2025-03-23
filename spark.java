5f016ed6bc369e3503df0b5fcd11bf7d620fb118
Shubham Pandey <shubhampan2222@gmail.com>
2025-03-23 16:33:52 +0530

    Dooone

--- spark.java
+++ spark.java
@@ -2,8 +2,13 @@ class spark{
     public static void main(String[] args) {
         System.out.println("Hello");
         m1();
+        paymet();
     }
     public static void m1(){
     System.out.println(".()");
+}
+
+public  static void paymet(){
+    System.out.println("Kl");
 }
 }
