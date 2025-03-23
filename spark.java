7329adb3e5bbcf6baf3bc9931ee2f8a32b0daf53
Shubham Pandey <shubhampan2222@gmail.com>
2025-03-23 16:59:51 +0530

    New service added

--- spark.java
+++ spark.java
@@ -3,12 +3,16 @@ class spark{
         System.out.println("Hello");
         m1();
         paymet();
+        payme();
     }
     public static void m1(){
     System.out.println(".()");
 }
 
 public  static void paymet(){
+    System.out.println("Kl");
+}
+public  static void payme(){
     System.out.println("Kl");
 }
 }
